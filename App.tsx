e58286a0840ad8b3c21c7a52aa50b0df846b5d9b
github username <github email address>
2020-04-27 10:42:23 -0400

    navigation 15

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, Image, StyleSheet, ImageBackground } from "react-native";
+import 'react-native-gesture-handler';
+import { View, Image, ImageBackground } from "react-native";
 import {
   Input,
   ThemeProvider,
@@ -8,106 +9,15 @@ import {
   Text,
 } from "react-native-elements";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        style={styles.background}
-        source={require("./src/images/f99ce9455f26958b1d98f21abfc9ac0f.gif")}
-      >
-        <View>
-          <Image
-            style={styles.logo}
-            source={require("./src/images/imageedit_2_6812498525.png")}
-          ></Image>
-        </View>
-        <View style={styles.inputView1}>
-          <Input placeholder="username" style={styles.input} />
-        </View>
-        <View style={styles.inputView2}>
-          <Input placeholder="password" style={styles.input} />
-        </View>
-        <View style={styles.loginBottom}></View>
-        <NavigationContainer>
-        <Button style={styles.loginButton} title="Log in" />
-        <Text h6 style={styles.signupMess}>
-            Don't have an account? Sign up for free.
-        </Text>
-          </NavigationContainer>
-      </ImageBackground>
-    </View>
+    <NavigationContainer>
+    
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "black",
-    alignItems: "center",
-    justifyContent: "center",
-    bottom: 2,
-  },
-  background: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-    width: "100%",
-    top: 4,
-  },
-  inputView1: {
-    position: "absolute",
-    top: 435,
-    left: "24%",
-    justifyContent: "center",
-    width: "50%",
-    borderRadius: 10,
-    backgroundColor: "gold",
-  },
-  inputView2: {
-    position: "absolute",
-    top: 495,
-    left: "24%",
-    justifyContent: "center",
-    width: "50%",
-    borderRadius: 10,
-    backgroundColor: "gold",
-  },
-  input: {
-    backgroundColor: "#3f51b5",
-    color: "green",
-  },
-  loginBottom: {
-    position: "absolute",
-    justifyContent: "center",
-    top: 700,
-    left: 100,
-    backgroundColor: "gold",
-    height: 2,
-    width: 205,
-  },
-  logo: {
-    position: "absolute",
-    bottom: 10,
-    left: "4%",
-    justifyContent: "center",
-    width: "100%",
-    height: 300,
-    borderRadius: 10,
-  },
-  loginButton: {
-    position: "absolute",
-    justifyContent: "center",
-    top: 115,
-    left: 140,
-    height: 100,
-    width: 120,
-  },
-  signupMess: {
-    position: "absolute",
-    justifyContent: "center",
-    color: "gold",
-    top: 720,
-    left: 70,
-  },
-});
---
+++ src/components/Login.js
@@ -0,0 +1,110 @@
+import * as React from 'react';
+import { Button, View, Text,StyleSheet} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function Login({ navigation }) {
+  return (
+    <View style={styles.container}>
+    <ImageBackground
+      style={styles.background}
+      source={require("./src/images/ezgif-7-8a3ab76e99bc.gif")}
+    >
+      <View>
+        <Image
+          style={styles.logo}
+          source={require("./src/images/imageedit_2_6812498525.png")}
+        ></Image>
+      </View>
+      <View style={styles.inputView1}>
+        <Input placeholder="username" style={styles.input} />
+      </View>
+      <View style={styles.inputView2}>
+        <Input placeholder="password" style={styles.input} />
+      </View>
+      <View style={styles.loginBottom}></View>
+     
+      <Button style={styles.loginButton} title="Log in" onPress={() => navigation.navigate('Home')} />
+      <Text h6 style={styles.signupMess}>
+          Don't have an account? Sign up for free.
+      </Text>
+        
+    </ImageBackground>
+  </View>
+      
+     
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+    bottom: 2,
+  },
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    width: "100%",
+    top: 4,
+  },
+  inputView1: {
+    position: "absolute",
+    top: 435,
+    left: "24%",
+    justifyContent: "center",
+    width: "50%",
+    borderRadius: 10,
+    backgroundColor: "gold",
+  },
+  inputView2: {
+    position: "absolute",
+    top: 495,
+    left: "24%",
+    justifyContent: "center",
+    width: "50%",
+    borderRadius: 10,
+    backgroundColor: "gold",
+  },
+  input: {
+    backgroundColor: "#3f51b5",
+    color: "green",
+  },
+  loginBottom: {
+    position: "absolute",
+    justifyContent: "center",
+    top: 700,
+    left: 100,
+    backgroundColor: "gold",
+    height: 2,
+    width: 205,
+  },
+  logo: {
+    position: "absolute",
+    bottom: 10,
+    left: "4%",
+    justifyContent: "center",
+    width: "100%",
+    height: 300,
+    borderRadius: 10,
+  },
+  loginButton: {
+    position: "absolute",
+    justifyContent: "center",
+    top: 115,
+    left: 140,
+    height: 100,
+    width: 120,
+  },
+  signupMess: {
+    position: "absolute",
+    justifyContent: "center",
+    color: "gold",
+    top: 720,
+    left: 70,
+  },
+});
+
